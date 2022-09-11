fd8c0c7b92f36b1475e037795591d1b5801afed4
Christopher Díaz <nealthmc@gmail.com>
2022-09-11 02:09:40 -0500

    add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import './styles/index.css';
 
 @Options({
   components: {
