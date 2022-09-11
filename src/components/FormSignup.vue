890b78a0788a7799f44b62d328e9d0d33dce4664
Christopher Díaz <nealthmc@gmail.com>
2022-09-11 09:04:57 -0500

    connect with the api

--- src/components/FormSignup.vue
+++ src/components/FormSignup.vue
@@ -1,19 +1,20 @@
 <template>
   <form @submit="handleSubmit" class="bg-black rounded-xl shadow-primary min-h-[600px] p-10">
-    <h2 class="text-primary text-3xl font-semibold text-center mb-10">
-      Signup
-    </h2>
+    <div v-if="!success">
+      <h2 class="text-primary text-3xl font-semibold text-center mb-10">
+        Signup
+      </h2>
 
-    <div class="text-white text-lg mb-10">
-      <div class="block mb-5">
-        <div class="text-md mb-1 text-primary">Name</div>
-        <label for="name">
-          <input
-            v-model="form.name.value"
-            type="text"
-            name="name"
-            placeholder="Adam Smith"
-            class="
+      <div class="text-white text-lg mb-10">
+        <div class="block mb-5">
+          <div class="text-md mb-1 text-primary">Name</div>
+          <label for="name">
+            <input
+              v-model="form.name.value"
+              type="text"
+              name="name"
+              placeholder="Adam Smith"
+              class="
               outline-none
               bg-transparent
               border-content
@@ -22,20 +23,20 @@
               p-2.5
               w-full
             "
-          />
-        </label>
-        <p class="text-secondary text-sm mt-2">{{ form.name.error }}</p>
-      </div>
+            />
+          </label>
+          <p class="text-secondary text-sm mt-2">{{ form.name.error }}</p>
+        </div>
 
-      <div class="block mb-5">
-        <div class="text-md mb-1 text-primary">Email</div>
-        <label for="email">
-          <input
-            v-model="form.email.value"
-            type="email"
-            name="email"
-            placeholder="email@gmail.com"
-            class="
+        <div class="block mb-5">
+          <div class="text-md mb-1 text-primary">Email</div>
+          <label for="email">
+            <input
+              v-model="form.email.value"
+              type="email"
+              name="email"
+              placeholder="email@gmail.com"
+              class="
               outline-none
               bg-transparent
               border-content
@@ -44,19 +45,19 @@
               p-2.5
               w-full
             "
-          />
-        </label>
-        <p class="text-secondary text-sm mt-2">{{ form.email.error }}</p>
-      </div>
+            />
+          </label>
+          <p class="text-secondary text-sm mt-2">{{ form.email.error }}</p>
+        </div>
 
-      <div class="block mb-5">
-        <div class="text-md mb-1 text-primary">Birthday</div>
-        <label for="birthday">
-          <input
-            v-model="form.birthday.value"
-            type="date"
-            name="birthday"
-            class="
+        <div class="block mb-5">
+          <div class="text-md mb-1 text-primary">Birthday</div>
+          <label for="birthday">
+            <input
+              v-model="form.birthday.value"
+              type="date"
+              name="birthday"
+              class="
               outline-none
               bg-transparent
               border-content
@@ -65,19 +66,19 @@
               p-2.5
               w-full
             "
-          />
-        </label>
-      </div>
+            />
+          </label>
+        </div>
 
-      <div class="block mb-5">
-        <div class="text-md mb-1 text-primary">Favorite Color</div>
-        <label for="color">
-          <input
-            v-model="form.color.value"
-            type="text"
-            name="color"
-            placeholder="Blue"
-            class="
+        <div class="block mb-5">
+          <div class="text-md mb-1 text-primary">Favorite Color</div>
+          <label for="color">
+            <input
+              v-model="form.color.value"
+              type="text"
+              name="color"
+              placeholder="Blue"
+              class="
               outline-none
               bg-transparent
               border-content
@@ -86,16 +87,22 @@
               p-2.5
               w-full
             "
-          />
-        </label>
-        <p class="text-secondary text-sm mt-2">{{ form.color.error }}</p>
-      </div>
+            />
+          </label>
+          <p class="text-secondary text-sm mt-2">{{ form.color.error }}</p>
+        </div>
 
+      </div>
+      <button
+        type="submit"
+        class="bg-primary p-2.5 rounded-xl w-full"
+      >Continue</button>
     </div>
-    <button
-      type="submit"
-      class="bg-primary p-2.5 rounded-xl w-full"
-    >Continue</button>
+    <div v-if="success" class="min-h-[inherit] flex items-center justify-center">
+      <h2 class="text-primary text-3xl font-semibold text-center mb-10">
+        Success!
+      </h2>
+    </div>
   </form>
 </template>
 
@@ -104,6 +111,8 @@ import { Vue } from 'vue-class-component';
 import regex from '@/constants/regex';
 
 export default class FormSignup extends Vue {
+  success = false
+
   form = {
     name: { value: '', error: '' },
     email: { value: '', error: '' },
@@ -121,9 +130,32 @@ export default class FormSignup extends Vue {
     const formHasError = name.error || email.error || birthday.error || color.error;
 
     if (!formHasError) {
-      // TODO: API Call.
-    } else {
-      // TODO: validate
+      this.saveUser();
+    }
+  }
+
+  saveUser = async () => {
+    const {
+      name, email, birthday, color,
+    } = this.form;
+
+    const request = await fetch(`${process.env.VUE_APP_API_URL}/api/user`, {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name.value,
+        email: email.value,
+        birthday: birthday.value,
+        favorite_color: color.value,
+      }),
+    });
+
+    const { success } = await request.json();
+    if (success) {
+      this.success = true;
     }
   }
 
